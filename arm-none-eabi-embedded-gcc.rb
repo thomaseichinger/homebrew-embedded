2a8a4c3893ffd7d04f5b93d0276c5973106b1541
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-02-06 12:46:40 +0100

    gcc arm embedded brings it own build scripts

--- arm-none-eabi-embedded-gcc.rb
+++ arm-none-eabi-embedded-gcc.rb
@@ -15,69 +15,80 @@ class ArmNoneEabiEmbeddedGcc < Formula
     
     target = 'arm-none-eabi'
     
-    gccbuildpath = buildpath
-    newlib = Formula.factory 'newlib'
-    newlib.brew do
-        ohai "Moving newlib into GCC build tree"
-        system "mv","newlib",gccbuildpath/"newlib"
-        ohai "Moving libgloss into GCC build tree"
-        system "mv","libgloss",gccbuildpath/"libgloss"
+    cd "src" do
+      system "find", ". -name '*.tar.*' | xargs -I% tar -xf %"
+      cd "zlib-1.2.5" do 
+        system "patch", "-p1 < ../zlib-1.2.5.patch"
+      end
     end
-
-    args = [
-        "--target=#{target}",
-        "--prefix=#{prefix}",
-        "--infodir=#{info}",
-        "--mandir=#{man}",
-        
-        "--enable-obsolete",
-        "--disable-nls",
-        "--disable-libssp",
-
-#       '--disable-werror',
-        
-        "--enable-languages=c,c++", 
-
-#       '--disable-decimal-float',
-        '--with-headers',
-        "--enable-interwork",
-        "--enable-multilib",
-        "--with-newlib",
-#       "--disable-newlib-supplied-syscalls",
-
-#       "--enable-newlib-io-long-long",
-#       "--enable-version-specific-runtime-libs",   
-#       "--enable-poison-system-directories",
-
-#       "--enable-target-optspace",
-
-#       "--without-included-gettext",
-#       "--disable-install-libiberty",
-#       "--disable-libunwind-exceptions",
-#       "--disable-libffi",
-#       '--disable-libmudflap',
-#       '--disable-libgomp',
-#       "--disable-__cxa_atexit",
-#       "--disable-libgfortran",
-#       "--disable-libssb",
-#       "--disable-libstdcxx-pch",
-
-        # Without this line, GCC won't be able to find the assembler.
-        "--with-as=#{binutils.prefix}/bin/#{target}-as",
-
-#       "--enable-newlib-io-reent-small",
-#       "--with-float=soft",
-#       "--with-abi=atpcs",     # Using old ABI for now.
-#       "--enable-float",
-#       "--enable-biendian",
-    ]
-
-    mkdir 'build' do
-        system '../configure', *args
-        system 'make all'
-        system 'make install'
-#       system 'make install-info'
-        rm_rf prefix/"lib/x86_64"
-    end
+    
+    system "./build-prerequisites.sh"
+    system "./build-toolchain.sh"
   end
 end
+
+ #    
+#     gccbuildpath = buildpath
+#     newlib = Formula.factory 'newlib'
+#     newlib.brew do
+#         ohai "Moving newlib into GCC build tree"
+#         system "mv","newlib",gccbuildpath/"newlib"
+#         ohai "Moving libgloss into GCC build tree"
+#         system "mv","libgloss",gccbuildpath/"libgloss"
+#     end
+# 
+#     args = [
+#         "--target=#{target}",
+#         "--prefix=#{prefix}",
+#         "--infodir=#{info}",
+#         "--mandir=#{man}",
+#         
+#         "--enable-obsolete",
+#         "--disable-nls",
+#         "--disable-libssp",
+# 
+# #       '--disable-werror',
+#         
+#         "--enable-languages=c,c++", 
+# 
+# #       '--disable-decimal-float',
+#         '--with-headers',
+#         "--enable-interwork",
+#         "--enable-multilib",
+#         "--with-newlib",
+# #       "--disable-newlib-supplied-syscalls",
+# 
+# #       "--enable-newlib-io-long-long",
+# #       "--enable-version-specific-runtime-libs",   
+# #       "--enable-poison-system-directories",
+# 
+# #       "--enable-target-optspace",
+# 
+# #       "--without-included-gettext",
+# #       "--disable-install-libiberty",
+# #       "--disable-libunwind-exceptions",
+# #       "--disable-libffi",
+# #       '--disable-libmudflap',
+# #       '--disable-libgomp',
+# #       "--disable-__cxa_atexit",
+# #       "--disable-libgfortran",
+# #       "--disable-libssb",
+# #       "--disable-libstdcxx-pch",
+# 
+#         # Without this line, GCC won't be able to find the assembler.
+#         "--with-as=#{binutils.prefix}/bin/#{target}-as",
+# 
+# #       "--enable-newlib-io-reent-small",
+# #       "--with-float=soft",
+# #       "--with-abi=atpcs",     # Using old ABI for now.
+# #       "--enable-float",
+# #       "--enable-biendian",
+#     ]
+# 
+#     mkdir 'build' do
+#         system '../configure', *args
+#         system 'make all'
+#         system 'make install'
+# #       system 'make install-info'
+#         rm_rf prefix/"lib/x86_64"
+#     end
