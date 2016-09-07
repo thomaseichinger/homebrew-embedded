24696b2ff37d9d9a4921866e10a6c42de07a278b
Thomas Eichinger <thomas.eichinger1@gmail.com>
2016-09-07 16:10:23 +0200

    update arm embedded gcc

--- arm-none-eabi-embedded-gcc.rb
+++ arm-none-eabi-embedded-gcc.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class ArmNoneEabiEmbeddedGcc < Formula
   homepage 'https://launchpad.net/gcc-arm-embedded'
-  version '20160330'
-  url 'https://launchpadlibrarian.net/251687676/gcc-arm-none-eabi-5_3-2016q1-20160330-mac.tar.bz2'
-  sha256 '480843ca1ce2d3602307f760872580e999acea0e34ec3d6f8b6ad02d3db409cf'
+  version '20160622'
+  url 'https://launchpadlibrarian.net/268330406/gcc-arm-none-eabi-5_4-2016q2-20160622-mac.tar.bz2'
+  sha256 'e175a0eb7ee312013d9078a5031a14bf14dff82c7e697549f04b22e6084264c8'
 
   def install
     ohai 'Copying binaries...'
